c84389e40d8748567174603987dde64e04cc6174
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2021-12-28 11:31:33 -0500

    Removing unused variables.

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,7 +5,6 @@ import PropTypes from 'prop-types';
 const Button = (props) => {
         const {...icons} = HeroIcons
         const Icon = icons[props.icon]
-        let icon;
         
         return (
             <button type="button" className={`focus:ring-${ props.color.toLowerCase() } shadow-sm appearance-none inline-flex cursor-pointer items-center px-${ props.padx } py-${ props.pady } hover:text-${ props.color.toLowerCase() }-500 text-${ props.color.toLowerCase() }-500 active:text-${ props.color.toLowerCase() }-500 text-sm font-medium active:bg-${ props.color.toLowerCase() }-100 bg-${ props.bg.toLowerCase() } hover:bg-${ props.color.toLowerCase() }-50 border focus:border-${ props.color.toLowerCase() }-500 focus:ring ring-${ props.color.toLowerCase() }-300 border-${ props.color.toLowerCase() }-300 rounded-md focus:outline-none transition`}>
