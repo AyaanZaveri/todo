c27abfdb37d734b71bf14c5c65c89b7b83eeab87
Ayaan Zaveri <63752541+AyaanZaveri@users.noreply.github.com>
2021-12-28 12:42:38 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const App = () => {
   return (
     <div className='flex justify-center mt-7 appearance-none'>
       <div className='flex items-start flex-col gap-y-3 appearance-none'>
-        <h1 className={`text-2xl w-80 font-bold text-${theme.toLowerCase()}-900 hover:text-${theme.toLowerCase()}-700 transition`}>To-Do List</h1>
+        <h1 className={`text-2xl w-80 font-bold text-${theme.toLowerCase()}-900 hover:text-${theme.toLowerCase()}-500 transition`}>To-Do List</h1>
 
         <Tasks color={theme} task={text} date={date} setText={setText} setDate={setDate} dateValue={date} />
 
